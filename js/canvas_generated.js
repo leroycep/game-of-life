987f4f55882e778d35c4fac235523b25195fba7f
leroycep <leroycepearson@geemili.xyz>
2020-07-25 06:51:33 +0000

    Deploying to gh-pages from  @ ebed0c0a71bdec92aed18fad5e7c4d50d9fb9ddd 🚀

--- js/canvas_generated.js
+++ js/canvas_generated.js
@@ -2,6 +2,8 @@ export default function getWebGLEnv(canvas_element, getMemory) {
     const utf8decoder = new TextDecoder();
     const readCharStr = (ptr, len) =>
         utf8decoder.decode(new Uint8Array(getMemory().buffer, ptr, len));
+    const readI32Array = (ptr, len) =>
+        new Uint32Array(getMemory().buffer, ptr, len);
     const writeCharStr = (ptr, len, lenRetPtr, text) => {
         const encoder = new TextEncoder();
         const message = encoder.encode(text);
@@ -21,6 +23,7 @@ export default function getWebGLEnv(canvas_element, getMemory) {
     const canvas = canvas_element.getContext('2d');
 
     const textAlignMap = ["left", "right", "center"];
+    const lineCapMap = ["butt", "round", "square"];
     return {
         getScreenW() {
             return canvas_element.getBoundingClientRect().width;
@@ -37,12 +40,37 @@ export default function getWebGLEnv(canvas_element, getMemory) {
         canvas_setFillStyle_rgba(r, g, b, a) {
             canvas.fillStyle = `rgba(${r},${g},${b},${a})`;
         },
+        canvas_setStrokeStyle_rgba(r, g, b, a) {
+            canvas.strokeStyle = `rgba(${r},${g},${b},${a})`;
+        },
         canvas_setTextAlign(text_align) {
             canvas.textAlign = textAlignMap[text_align];
+        },
+        canvas_setLineCap(line_cap) {
+            canvas.lineCap = lineCapMap[line_cap];
+        },
+        canvas_setLineWidth(width) {
+            canvas.lineWidth = width;
+        },
+        canvas_setLineDash_(segments_ptr, segments_len) {
+            const segments = readI32Array(segments_ptr, segments_len);
+            canvas.setLineDash(segments);
         },
         canvas_fillText_(text_ptr, text_len, x, y) {
             const text = readCharStr(text_ptr, text_len);
             canvas.fillText(text, x, y);
+        },
+        canvas_moveTo(x, y) {
+            canvas.moveTo(x, y);
+        },
+        canvas_lineTo(x, y) {
+            canvas.lineTo(x, y);
+        },
+        canvas_beginPath() {
+            canvas.beginPath();
+        },
+        canvas_stroke() {
+            canvas.stroke();
         },
     };
 }
