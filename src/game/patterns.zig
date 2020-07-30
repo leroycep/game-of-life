204bac7d6e6aa472588ffab140e949bdebfd3139
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-07-30 14:57:03 -0600

    Use `u1`s to make predefined patterns

--- src/game/patterns.zig
+++ src/game/patterns.zig
@@ -9,7 +9,7 @@ const vec2is = platform.vec2is;
 pub const Pattern = struct {
     name: []const u8,
     size: Vec(2, usize),
-    cells: []const bool,
+    cells: []const u1,
 
     pub fn check(self: @This()) !@This() {
         if (self.size.x() * self.size.y() != self.cells.len) {
@@ -24,7 +24,9 @@ pub const Pattern = struct {
             .edge_behaviour = .Dead,
         });
         errdefer grid.deinit(alloc);
-        std.mem.copy(bool, grid.cells, self.cells);
+        for (grid.cells) |*cell, idx| {
+            cell.* = self.cells[idx] == 1;
+        }
         return grid;
     }
 };
@@ -32,31 +34,31 @@ pub const Pattern = struct {
 pub const GLIDER = try Pattern.check(.{
     .name = "Glider",
     .size = vec2us(3, 3),
-    .cells = &[_]bool{
-        false, true,  true,
-        true,  false, true,
-        false, false, true,
+    .cells = &[_]u1{
+        0, 1, 1,
+        1, 0, 1,
+        0, 0, 1,
     },
 });
 
 pub const PULSAR = try Pattern.check(.{
     .name = "Pulsar",
     .size = vec2us(15, 15),
-    .cells = &[_]bool{
-        false, false, false, false, true,  false, false, false, false, false, true,  false, false, false, false,
-        false, false, false, false, true,  false, false, false, false, false, true,  false, false, false, false,
-        false, false, false, false, true,  true,  false, false, false, true,  true,  false, false, false, false,
-        false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
-        true,  true,  true,  false, false, true,  true,  false, true,  true,  false, false, true,  true,  true,
-        false, false, true,  false, true,  false, true,  false, true,  false, true,  false, true,  false, false,
-        false, false, false, false, true,  true,  false, false, false, true,  true,  false, false, false, false,
-        false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
-        false, false, false, false, true,  true,  false, false, false, true,  true,  false, false, false, false,
-        false, false, true,  false, true,  false, true,  false, true,  false, true,  false, true,  false, false,
-        true,  true,  true,  false, false, true,  true,  false, true,  true,  false, false, true,  true,  true,
-        false, false, false, false, false, false, false, false, false, false, false, false, false, false, false,
-        false, false, false, false, true,  true,  false, false, false, true,  true,  false, false, false, false,
-        false, false, false, false, true,  false, false, false, false, false, true,  false, false, false, false,
-        false, false, false, false, true,  false, false, false, false, false, true,  false, false, false, false,
+    .cells = &[_]u1{
+        0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+        0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+        0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1,
+        0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0,
+        0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0,
+        0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0,
+        1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0,
+        0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+        0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
     },
 });
